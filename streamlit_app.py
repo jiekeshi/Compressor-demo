e610f96ead75ba208f61916c4049b1af805c905c
jiekeshi <jiekeshi125@gmail.com>
2023-01-30 23:32:31 +0800

    chore: update examples

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,18 @@ Below are the examples of using different models to predict whether a given code
 """
 
 txt = st.text_area('Code to analyze', '''
-    aaaa
+    // An vulnerable example form ReVeal dataset
+    int getulong(const char * numstr, unsigned long int * result) {
+    long long int val;
+    char * endptr;
+    errno = 0;
+    val = strtoll(numstr, & endptr, 0);
+    if (('\\0' == * numstr) || ('\\0' != * endptr) || (ERANGE == errno) || (val != (unsigned long int) val)) {
+        return 0;
+    }
+    * result = (unsigned long int) val;
+    return 1;
+}
     ''')
 
 option = st.selectbox(
